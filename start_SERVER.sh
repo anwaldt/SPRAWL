fe673ea7693544eebaf7e1102d8736910c66dd2d
studio <studio@riviera.ak.tu-berlin.de>
2019-12-14 17:39:33 +0100

    sc-server: added parallel bus structures for pis and loudspeaker out!

--- start_SERVER.sh
+++ start_SERVER.sh
@@ -5,14 +5,12 @@
 killall jacktrip
 killall sclang
 killall scsynth
-
+sleep 2
 ################################
 
 rm log/SC.log
 
 sclang SC/sprawl_SERVER.sc > log/SC.log &
 
-for i in $(seq 0 15); do
-    rm log/poe_$i'.log'
-    ~/jacktrip-master-e85fb18fd143ced04384dd43bbac3fa9207cffd8/src/jacktrip -s -o $i -n 1 --clientname POE_$i > log/poe_$i'.log' &
-done
+~/Development/jacktrip-recent/src/jacktrip -S -p 3 -n 1 > log/jacktrip.log &
+
