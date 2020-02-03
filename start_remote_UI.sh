af7c5c63c7d6f7b03d1f7a8407731d603224bc25
studio <voncoler@tu-berlin.de>
2020-02-03 18:56:05 +0100

    Added remote GUI starter!

---
+++ start_remote_UI.sh
@@ -0,0 +1,10 @@
+ssh -t pi@192.168.0.101 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.102 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.103 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.104 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.105 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.106 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.26 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.108 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.109 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
+ssh -t pi@192.168.0.110 'export DISPLAY=:0.0; ./disable_screen_blanking.sh; cd ~/concert/sprawl && nohup ./start_CLIENT.sh' &
