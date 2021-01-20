0598d2f8fa84106b18cd6a5ca5456a8c7416d135
Nils Tonnätt <nils.tonnaett@campus.tu-berlin.de>
2021-01-20 14:49:27 +0100

    Don't ask for buffer size / Default to 128

--- start_jack_client.sh
+++ start_jack_client.sh
@@ -3,10 +3,10 @@
 killall jacktrip
 killall jackd
 
-BUFFER=$(zenity --list \
-column=Check --height 800 \
---text="Please select a buffer size by checking only one number! Ask your session admin for details!" \
---column="Select" --column="Buffersize"  64 FALSE 128 FALSE 256 FALSE 512 FALSE 1024   --checklist);
+#BUFFER=$(zenity --list \
+#column=Check --height 800 \
+#--text="Please select a buffer size by checking only one number! Ask your session admin for details!" \
+#--column="Select" --column="Buffersize"  64 FALSE 128 FALSE 256 FALSE 512 FALSE 1024   --checklist);
 
 echo $BUFFER
 
@@ -16,7 +16,7 @@ echo $BUFFER
 
 sleep 1
 
-jackd -P 90 -d alsa -d hw:1,0 -r 48000 -p $BUFFER &
+jackd -P 90 -d alsa -d hw:1,0 -r 48000 -p 128 &
 
 sleep 2
 
