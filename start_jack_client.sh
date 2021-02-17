a403db6f4d6b49cd9e6b11dd0bba59f07c8524dd
Nils Tonnätt <nils.tonnaett@campus.tu-berlin.de>
2021-02-17 12:51:43 +0100

    Ask for number of outgoing channels

--- start_jack_client.sh
+++ start_jack_client.sh
@@ -3,12 +3,10 @@
 killall jacktrip
 killall jackd
 
-#BUFFER=$(zenity --list \
-#column=Check --height 800 \
-#--text="Please select a buffer size by checking only one number! Ask your session admin for details!" \
-#--column="Select" --column="Buffersize"  64 FALSE 128 FALSE 256 FALSE 512 FALSE 1024   --checklist);
+OUTCHANS=$(zenity --list --radiolist \
+--text="Please select the number of outgoing channels to the network!" \
+--column="Select" --column="Outgoing Channels"  FALSE "1" TRUE "2");
 
-echo $BUFFER
 
 # sleep 1
 
@@ -20,4 +18,4 @@ jackd -P 90 -d alsa -d hw:1,0 -r 48000 -p 128 &
 
 sleep 2
 
-bin/jacktrip_nils -C 85.214.78.6 -n 2 -K AP
+bin/jacktrip_nils -C 85.214.78.6 --numincoming 2 --numoutgoing $OUTCHANS -K AP
