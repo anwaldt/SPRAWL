6471cf556f4ae856bd1df336e05e2b5117f0d308
Q3-113 <simonoswald@outlook.com>
2021-05-15 16:36:47 +0200

    one source can be controlled through the GUI, scale can be set as cmdl arg

--- Python/visual_positions.py
+++ Python/visual_positions.py
@@ -17,6 +17,8 @@ OFFSET = SCREEN_SIZE // 2
 SCALE = 50
 DEFAULT_SIZE = 20
 
+CTRL = 0
+
 # OSC dispatcher must be global
 dispatcher = dispatcher.Dispatcher()
 
@@ -42,14 +44,14 @@ class Canvas(tk.Frame):
 
     self._drag_data = {"x": 0, "y": 0, "item": None}
 
-    # add bindings for clicking, dragging and releasing over
-    # any object with the "token" tag
+    # bindings for clicking, dragging and releasing
     self.canvas.tag_bind("token", "<ButtonPress-1>", self.drag_start)
     self.canvas.tag_bind("token", "<ButtonRelease-1>", self.drag_stop)
     self.canvas.tag_bind("token", "<B1-Motion>", self.drag)
 
     # create controllable source
-    self.canvas.create_circle(OFFSET + 30, OFFSET, 20, fill="sky blue", tag="token")
+    if CTRL >= 0:
+      self.canvas.create_circle(OFFSET, OFFSET, 20, fill="sky blue", tag="token")
 
     self.run(self.canvas)
 
@@ -64,12 +66,13 @@ class Canvas(tk.Frame):
   def draw_grid(self, canvas):
     w = canvas.winfo_width()
     h = canvas.winfo_height()
+    grid_color = 'snow3'
 
     for i in range(0, w, SCALE):
-      canvas.create_line([(i, 0), (i, h)], tag='grid_line')
+      canvas.create_line([(i, 0), (i, h)], tag='grid_line', fill=grid_color)
 
     for i in range(0, h, SCALE):
-      canvas.create_line([(0, i), (w, i)], tag='grid_line')
+      canvas.create_line([(0, i), (w, i)], tag='grid_line', fill=grid_color)
 
 
   def draw_players(self, canvas):
@@ -80,23 +83,29 @@ class Canvas(tk.Frame):
                              self.check_range(DEFAULT_SIZE + elev, 10, 30)]
 
     for i, [x, y, r] in enumerate(self.players):
-      if i == 0:
+      if i == CTRL:
         pass
+        canvas.create_text(x, y, text=i, tag='ctrl_index')
         # canvas.create_circle(x, y, r, fill="sky blue", tag="token")
       else:
         canvas.create_circle(x, y, r,fill='pink', tag='player')
-      
-      canvas.create_text(x, y, text=i, tag='player')
+        canvas.create_text(x, y, text=i, tag='player_index')
 
     
   def run(self, canvas):
     # TODO: only redraw grid after window resize
     #       otherwise only delete 'players' tag
     canvas.delete('player')
+    canvas.delete('player_index')
+    canvas.delete('ctrl_index')
     canvas.delete('grid')
     
     self.draw_grid(canvas)
     self.draw_players(canvas)
+
+    canvas.tag_raise('token')
+    canvas.tag_raise('ctrl_index')
+
     self.after(10, self.run, canvas)
 
   def drag_start(self, event):
@@ -141,10 +150,8 @@ def osc_send(ip, port):
     [x, y] = send.get()
     dist = math.sqrt(x*x + y*y)
     azim = math.atan2(y, x)
-    client.send_message("/source/azim", [0, azim])
-    client.send_message("/source/dist", [0, dist])
-    # time.sleep(.5)
-
+    client.send_message("/source/azim", [CTRL, azim])
+    client.send_message("/source/dist", [CTRL, dist])
 
 # callback, run when new OSC position signal is received
 def update_pos(_addr, index, azim, elev, dist):
@@ -158,12 +165,15 @@ def update_pos(_addr, index, azim, elev, dist):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()        
     
-  parser.add_argument("-c", "--controlled-source", dest="controlled_source", default="0", help="Index of the source you want to control")
+  parser.add_argument("-c", "--controlled-source", dest="controlled_source", default="-1", help="Index of the source you want to control")
   parser.add_argument("-i", "--listen-ip", dest="listen_ip", default='127.0.0.1', help="IP of the server sending the position data")
   parser.add_argument("-p", "--listen-port", dest="listen_port", default=5003, help="Port of the server sending the position data")
-
+  parser.add_argument("-s", "--scale", dest="scale", default=50, help="set the zoom level")
 
   args = parser.parse_args()
+
+  CTRL = int(args.controlled_source)
+  SCALE = int(args.scale)
 
   # listen for new signals in seperate thread
   listener = threading.Thread(target=osc_listen, args=(args.listen_ip, args.listen_port))
